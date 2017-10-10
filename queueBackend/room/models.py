9bbac67c725c96f8363e0b8d891152ab622a0fa7
DerrickChanCS <dchan1@scu.edu>
2017-10-09 21:57:43 -0700

    updated model view

--- queueBackend/room/models.py
+++ queueBackend/room/models.py
@@ -3,4 +3,10 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Room(models.Model):
+    code = models.CharField(max_length=4)
+
+class Question(models.Model):
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    tag =  models.CharField(max_length=20)
+
