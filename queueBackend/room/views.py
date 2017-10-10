474edc2fdf79e49f663492dfaa137852bfd60b8a
DerrickChanCS <dchan1@scu.edu>
2017-10-09 22:43:45 -0700

    Added room creation code. Generates a random 4 letter code. Built basic database functionality

--- queueBackend/room/views.py
+++ queueBackend/room/views.py
@@ -8,3 +8,14 @@ from django.shortcuts import render
 
 def index(request):
     return HttpResponse("Hello, world. You're at the room index.")
+
+def create(request):
+    import random
+    alpha = ("A", "B", "C", "D", "E",
+      "F", "G", "H", "I", "J",
+      "K", "L", "M", "N", "O",
+      "P", "Q", "R", "S", "T",
+      "U", "V", "W", "X", "Y",
+      "Z", )
+    code = ''.join([random.choice(alpha) for _ in range(4)])
+    return HttpResponse("Creating room. Code "+code)
