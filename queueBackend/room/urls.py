46934b9f61b4b77c3fe59d54935de409720f6d4a
DerrickChanCS <dchan1@scu.edu>
2017-10-09 23:20:48 -0700

    Added view to manual specify room code.

--- queueBackend/room/urls.py
+++ queueBackend/room/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'create/', views.create, name='create'),
+    url(r'delete/', views.delete, name='delete'),
+    url(r'^createRoom/(?P<roomid>[A-Z]+)/$', views.createCode, name='createCode'),
 ]
