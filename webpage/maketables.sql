727c01b103c0cd1dc77ff79e8fbe9a10df293d47
GeromeYabut <gerome.yabut@gmail.com>
2017-10-27 10:47:19 -0700

    Add files via upload

--- webpage/maketables.sql
+++ webpage/maketables.sql
@@ -1,5 +1,13 @@
 CREATE TABLE Rooms(
-    code char(5) PRIMARY KEY
+    code char(5) PRIMARY KEY,
+);
+
+CREATE TABLE Tags(
+    room char(5),
+    tagname char(20),
+    FOREIGN KEY (room)
+    	REFERENCES Rooms(code)
+    	ON DELETE CASCADE
 );
 
 CREATE TABLE Questions(
