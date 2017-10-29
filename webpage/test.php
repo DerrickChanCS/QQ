67cb3ca9c7d3ec09aa4f0a3271db9f563a2d08cc
Mycul <enriquez_michael@verizon.net>
2017-10-28 22:24:51 -0700

    Add files via upload
    Have to add functionality to remove the extra created divs in the close question function, currently only really works as a demo to show the questions getting added, removing right now leaves a superfluous div, will work on this promptly

---
+++ webpage/test.php
@@ -0,0 +1,18 @@
+<?php
+$q = $_REQUEST["q"];
+?>
+<div class="w3-row" style="margin-left: 15%; margin-top: 30px">
+     <div class="w3-panel w3-grey w3-display-container w3-dropdown-click w3-animate-opacity" style="height: 100px; width: 70%">
+            <div onclick="expandQuestion('Q<?php echo $q; ?>')" class="w3-display-left w3-dark-grey w3-center" style="width: 50%; height: 100px">
+                <p>Test Station <?php echo $q; ?></p>
+            </div>
+            <div class="w3-display-right w3-grey w3-center" style="width: 50%">
+                <p>Test Tag</p>
+                <p>Timestamp</p>
+            </div>
+            <span onclick="closeQuestion('Q<?php echo $q; ?>'), this.parentElement.style.display = 'none'"
+                  class="w3-button w3-border w3-dark-grey w3-large w3-display-topright" style="height: 51px">&times;</span>
+        </div>
+        <textarea id="Q<?php echo $q; ?>" class="w3-border w3-hide w3-container w3-animate-opacity" rows="4" readonly="" style="height: 100px; width: 59.5%; margin-bottom: 20px">SampleText question content Number 2
+        </textarea>
+</div>
