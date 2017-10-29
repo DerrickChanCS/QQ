6d6e02a2388e83c71d8b5d682494fd80c960cfd4
Mycul <enriquez_michael@verizon.net>
2017-10-28 23:31:49 -0700

    Add files via upload
    Oh lets go question text pops up now and I got rid of the extra spacing issue. Actually looks semi functional now. Granted its not. From here we need to also send the text content and all the other whatever info to the database so that the TA view is able to view it concurrently on their view. Creating the TA view of this should be relatively simple since its just a different version of the student page.

--- webpage/test.php
+++ webpage/test.php
@@ -1,7 +1,8 @@
 <?php
 $q = $_REQUEST["q"];
+$t = $_REQUEST["t"];
 ?>
-<div class="w3-row" style="margin-left: 15%; margin-top: 30px">
+<div id="M<?php echo $q; ?>"class="w3-row" style="margin-left: 15%; margin-top: 30px">
      <div class="w3-panel w3-grey w3-display-container w3-dropdown-click w3-animate-opacity" style="height: 100px; width: 70%">
             <div onclick="expandQuestion('Q<?php echo $q; ?>')" class="w3-display-left w3-dark-grey w3-center" style="width: 50%; height: 100px">
                 <p>Test Station <?php echo $q; ?></p>
@@ -10,9 +11,9 @@ $q = $_REQUEST["q"];
                 <p>Test Tag</p>
                 <p>Timestamp</p>
             </div>
-            <span onclick="closeQuestion('Q<?php echo $q; ?>'), this.parentElement.style.display = 'none'"
+            <span onclick="closeQuestion('Q<?php echo $q; ?>'), this.parentElement.style.display = 'none', this.parentElement.parentElement.style.display = 'none'"
                   class="w3-button w3-border w3-dark-grey w3-large w3-display-topright" style="height: 51px">&times;</span>
         </div>
-        <textarea id="Q<?php echo $q; ?>" class="w3-border w3-hide w3-container w3-animate-opacity" rows="4" readonly="" style="height: 100px; width: 59.5%; margin-bottom: 20px">SampleText question content Number 2
+        <textarea id="Q<?php echo $q; ?>" class="w3-border w3-hide w3-container w3-animate-opacity" rows="4" readonly="" style="height: 100px; width: 59.5%; margin-bottom: 20px"><?php echo $t; ?>
         </textarea>
 </div>
